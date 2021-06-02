0be84e82542a47f4a76282c8beee018455354db3
mrafiq709 <mrafiq709@gmail.com>
2021-06-02 09:01:32 +0700

    find bear in background

--- bear/popup.js
+++ bear/popup.js
@@ -1,24 +1,31 @@
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    chrome.tabs.query(
-      {
-        active: true,
-        lastFocusedWindow: true,
-      },
-      function (tabs) {
-        // and use that tab to fill in out title and url
-        let tab = tabs[0];
-        chrome.tabs.sendMessage(tab.id, "Hi", setCount);
-        // alert(tab.url);
-      }
-    );
+    // chrome.tabs.query(
+    //   {
+    //     active: true,
+    //     lastFocusedWindow: true,
+    //   },
+    //   function (tabs) {
+    //     // and use that tab to fill in out title and url
+    //     let tab = tabs[0];
+    //     chrome.tabs.sendMessage(tab.id, "Hi", setCount);
+    //     // alert(tab.url);
+    //   }
+    // );
 
-    function setCount(res) {
+    // function setCount(res) {
+    //   const div = document.createElement("div");
+    //   div.textContent = `${res.count} bears`;
+    //   document.body.appendChild(div);
+    // }
+
+    const bg = chrome.extension.getBackgroundPage();
+    Object.keys(bg.bears).forEach(function (url) {
       const div = document.createElement("div");
-      div.textContent = `${res.count} bears`;
+      div.textContent = `${url}: ${bg.bears[url]}`;
       document.body.appendChild(div);
-    }
+    });
   },
   false
 );
